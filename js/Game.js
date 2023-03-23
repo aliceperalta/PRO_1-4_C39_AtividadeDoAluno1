e80c0796ac1eeeade2c5fbb0f2e43223019caf4a
aliceperalta <114191645+aliceperalta@users.noreply.github.com>
2023-03-23 18:59:50 -0300

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,30 @@ class Game {
   play() {
     this.handleElements();
 
-    //Player.getPlayersInfo();
+    Player.getPlayersInfo(); //added
+    
+    if (allPlayers !== undefined) { //se todos os jogadores ja estiverem definidos era exibir a pista e os carros 
+      image(track, 0, -height * 5, widht, height * 6);
+      //ao exibir a imagem, manteremos a posição x como 0 e y como (-height * 5); isso criara a pista fora da tela
+      //porque não queremos mostrar a pista completa de uma vez, a pista sera exibida conforme o jogador mover o carro.
+      //indice do sjogadores inicia em 0. O primeiro recebe o numero 1. O segundo numero 2.
+      var index = 0;
+      for ( var plr in allPlayers {
+      //plr é a variavel usada para atravessae, e allPlayers é o objeto JavaScript no qual procuramos os valores 
+      //ou seja, para realizar o posicionamento e todas as informações de todos os jogadores
+      var x = allPlayers[plr].positionX;
+      var y = height - allPlayers[plr].positionY;
+      cars[index].position.x = x;
+      cars[index].position.y = y;
+      //adiciona um ao indice anterior
+      ndex = index + 1;
+      }
+      //se a tecla pessionada para baixo é a SETA PARA CIMA
+      if (keyIsDown(UP_ARROW)) {
+        player.positionY += 10; //aumente a posição do jogador de 10 em 10 a cada toque
+        player.update(); //atualize a posição no banco de dados 
+       }
+      
 
    
       drawSprites();
